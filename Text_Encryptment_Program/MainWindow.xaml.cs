74f69bb0e1595506ab2eab85abfb3ea750b3876f
Mullins182 <Mullins182@t-online.de>
2024-01-27 21:38:11 +0100

    file dialog modified so the initial directory for file selection is always the working directory.

--- Text_Encryptment_Program/MainWindow.xaml.cs
+++ Text_Encryptment_Program/MainWindow.xaml.cs
@@ -34,6 +34,7 @@ namespace Text_Encryptment_Program
             dialog.FileName = "Your File !"; // Default file name
             dialog.DefaultExt = ".txt"; // Default file extension
             dialog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+            dialog.InitialDirectory = Environment.CurrentDirectory; // Sets the initial Dir for File Dialog Box to actual working Directory !
 
             // Show open file dialog box
             bool? result = dialog.ShowDialog();
@@ -41,10 +42,10 @@ namespace Text_Encryptment_Program
             // Process open file dialog box results
             if (result == true)
             {
-                //// Open document
-                //string filename = dialog.FileName;
+                // Open document
+                string filename = dialog.FileName;
 
-                TextData = LoadContentIntoDecryptedText.ReadFileData(dialog.FileName);
+                TextData = LoadContentIntoDecryptedText.ReadFileData(filename);
 
                 foreach (var item in TextData)
                 {
