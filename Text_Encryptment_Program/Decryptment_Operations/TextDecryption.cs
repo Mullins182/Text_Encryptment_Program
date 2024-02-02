52f3f4e218b5abd487285c16a85af237271110c8
Mullins182 <Mullins182@t-online.de>
2024-02-02 10:48:13 +0100

    debugging decryption method (not working atm)

--- Text_Encryptment_Program/Decryptment_Operations/TextDecryption.cs
+++ Text_Encryptment_Program/Decryptment_Operations/TextDecryption.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Text_Encryptment_Program
 {
@@ -10,22 +11,30 @@ namespace Text_Encryptment_Program
     {
         public TextDecryption() { }
 
-        public List<string> DecryptText(List<string> text, Dictionary<int, int> keyList, int key) 
+        public List<string> DecryptText(List<string> encryptedText, Dictionary<int, int> keyList, int key) 
         {
             List<string> result = new List<string>();
-            string cache;
+            //List<string> textList = new List<string>();
+            string cache = "";
+            //string cache2 = "";
 
-            foreach (var item in text)
+            foreach (var item in encryptedText)
             {
-                for (int i = key; i < 126; i++)
+                for (int i = 32; i < 127; i++)
                 {
                     cache = item.Replace(Convert.ToChar(keyList[i]), Convert.ToChar(i));
-                    //cache = Convert.ToString(keyList[i]);
-                    result.Add(cache);
                 }
+                result.Add(cache);
             }
 
             return result;
         }
+
+        //public string DecryptedString(string text, Dictionary<int, int> keys) 
+        //{
+        //    text.Replace(Convert.ToChar(keys[32]), Convert.ToChar(32));
+
+        //    return text;
+        //}
     }
 }
